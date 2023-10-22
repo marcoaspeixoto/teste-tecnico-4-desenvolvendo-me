43921e94881d0c1024836b1f04605bf77337662b
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-10-22 12:53:36 -0300

    test(diamonds): in operation

--- spec/business/extrair_diamantes_spec.rb
+++ spec/business/extrair_diamantes_spec.rb
@@ -1,21 +1,16 @@
 require_relative '../../app/business/extrair_diamantes_e_areias'
 
 RSpec.describe 'contar_e_extrair_diamantes' do
-  it 'deve contar e extrair diamantes corretamente' do
+  it 'deve extrair areias e contar diamantes corretamente' do
     expressao = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
     quantidade_diamantes = contar_e_extrair_diamantes(expressao)
 
-    # Verifique a quantidade correta de diamantes
     expect(quantidade_diamantes).to eq(7)
 
-    # Verifique a expressão após a extração
-    expect(expressao).to eq('')
-
     # Outro exemplo
-    expressao2 = '<<<>>><<<>>>'
+    expressao2 = '<<<>.>.><<<>>>'
     quantidade_diamantes2 = contar_e_extrair_diamantes(expressao2)
 
-    expect(quantidade_diamantes2).to eq(3)
-    expect(expressao2).to eq('')
+    expect(quantidade_diamantes2).to eq(2)
   end
 end
