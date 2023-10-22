5f93707cbdbbd7a845edb2a2b75f74f6d7d3e2da
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-10-22 12:50:38 -0300

    feat(diamonds): deleting the new expression from the display

--- app/business/extrair_diamantes_e_areias.rb
+++ app/business/extrair_diamantes_e_areias.rb
@@ -1,15 +1,12 @@
 def contar_e_extrair_diamantes(expressao)
   diamantes = 0
 
-  expressao_copy = expressao.dup  # Cria uma cópia da expressao para percorrer
+  # Remove todos os pontos (.)
+  expressao.gsub!('.', '')
 
-  # Remove todos os pontos (.) da cópia.
-  expressao_copy.gsub!('.', '')
-
-  # Conta os diamantes na cópia e extrai um por vez da expressao original.
-  expressao_copy.scan(/<[^<>]*>/) do |match|
+  # Conta os diamantes
+  expressao.scan(/<[^<>]*>/) do |match|
     diamantes += 1
-    expressao.sub!(match, '')  # Substitui o diamante na expressao original.
   end
 
   diamantes
@@ -20,4 +17,3 @@ expressao = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
 quantidade_diamantes = contar_e_extrair_diamantes(expressao)
 
 puts "Quantidade de diamantes extraídos: #{quantidade_diamantes}"
-puts "Expressão após extração: #{expressao}"
