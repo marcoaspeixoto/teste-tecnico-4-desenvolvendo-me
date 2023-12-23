cc3e98e8dd67d69127ef201377ec366cb2e612ea
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-12-23 19:51:18 -0300

    fix: sort by year

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :movies do
     collection do
-      post 'import_csv' # Rota para importar o CSV
+      post 'import_csv', to: 'movies#import_csv' # Rota para importar o CSV
+      get 'sort_by_year', to: 'movies#sort_by_year' # ordenar pelo ano de nascimento
       get 'filter_by_year', to: 'movies#filter_by_year' # Rota para filtrar por ano
       get 'filter_by_media_type', to: 'movies#filter_by_media_type' # Rota para filtrar por tipo de mídia
       get 'filter_by_country', to: 'movies#filter_by_country' # Rota para filtrar por país
